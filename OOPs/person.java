6a62fa3c6e65398d42be18c7c8f40d10e3c2d6e7
Isha-lad <71178848+Isha-lad@users.noreply.github.com>
2022-06-23 20:54:42 +0530

    Create person.java

---
+++ OOPs/person.java
@@ -0,0 +1,13 @@
+
+public class person
+{  
+    void run()
+    {
+        System.out.println("running");
+    }   
+public static void main(String args[])
+{  
+    person b = new person();
+    b.run();  
+} 
+}

2d8fa24a679dbe1fcfb2476269c976a5affe1d6b
Isha-lad <71178848+Isha-lad@users.noreply.github.com>
2022-06-30 08:46:36 +0530

    Create book.java

---
+++ Assignment/library_management_system/book.java
@@ -0,0 +1,61 @@
+public class book {
+    public void religious() {
+        String[] rel = {"Ramayan", "Mahabharat"};
+
+        System.out.println("Book available");
+        for (String s : rel) {
+            System.out.println(s);
+        }
+    }
+
+    public void educational() {
+
+        String[] edu = {"maths", "english", "science"};
+
+        System.out.println("Book available");
+        for (String s : edu) {
+            System.out.println(s);
+        }
+    }
+
+    public void romantic() {
+
+        String[] rom = {"romeo & juliet"};
+
+        System.out.println("Book available");
+        for (String s : rom) {
+            System.out.println(s);
+        }
+    }
+
+    public void comic() {
+
+        String[] com = {"Vikram-Betaal"};
+
+        System.out.println("Book available");
+        for (String s : com) {
+            System.out.println(s);
+        }
+    }
+
+    public void thriller() {
+
+        String[] thr = {"The Firm"};
+
+        System.out.println("Book available");
+        for (String s : thr) {
+            System.out.println(s);
+        }
+    }
+
+    public void self_help() {
+
+        String[] sel = {"Rich Dad Poor Dad"};
+
+        System.out.println("Book available");
+        for (String s : sel) {
+            System.out.println(s);
+        }
+    }
+}
+
